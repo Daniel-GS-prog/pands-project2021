4e3c6b5d4d2a8420508920758605c078dd702b0e
Daniel Gonzalez <daniel_gonzalez_sanchez@yahoo.com>
2021-04-01 17:34:19 +0100

    Adding summary, text.file and summary table

--- analysis.py
+++ analysis.py
@@ -13,6 +13,17 @@ dataset = pd.read_csv("iris.data.csv")
 dataset.columns = ["sepal length", "sepal width", "petal lenght", "petal width", "variety"]
 # Definning attributes
 
-print(dataset.head(1))
+#print(dataset.head(1))
 #prints 1 instance as a sample
+
+variable_summary = dataset.describe() 
+#print(variable_summary)
+
+variable_summary_t = variable_summary.transpose() 
+# Prints a summary of each variable
+
+#print(dataset.groupby("variety").size())
+
+#variable_summary_t.to_csv("summary.txt")
+
 
