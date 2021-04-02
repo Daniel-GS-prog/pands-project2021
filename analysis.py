cf95f1a7bec85d09cd4de204342c2cd27cf77b00
Daniel Gonzalez <daniel_gonzalez_sanchez@yahoo.com>
2021-04-02 16:03:55 +0100

    Updated with data visualisation

--- analysis.py
+++ analysis.py
@@ -2,14 +2,16 @@
 # Author: Daniel Gonzalez
 
 
-import numpy as np
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+
+### --- Description:
+
 dataset = pd.read_csv("iris.data.csv", header=None, names=["sepal length", "sepal width", "petal lenght", "petal width", "variety"])
 # Reads data from file
-
 
 print(dataset.head(1))
 #prints 1 instance as a sample
@@ -17,34 +19,41 @@ print(dataset.head(1))
 variable_summary = dataset.groupby("variety").describe()
 # Describes all variables grouped by variety of flower
 
-#variable_summary.to_csv("summary.txt")
+variable_summary.to_csv("summary.txt")
 #saves the summary as a text file
 
-#variable_summary.to_csv("summary_table.txt")
+variable_summary.to_csv("summary_table.txt")
 # Copy of the file as a readable table
 
-setosa = dataset.loc[:49]
-versicolor = dataset.loc[49:99]
-virginica = dataset.loc[99:]
-# Setting indexes for each variety
 
-""" s = setosa.describe()
-ver = versicolor.describe()
-vir = virginica.describe() """
-# Defines a descrive function to each variety
+# Accessing data grouped by each variety
+setosa = dataset[dataset["variety"] =="Iris-setosa"]
+versicolor = dataset[dataset["variety"] =="Iris-versicolor"]
+virginica = dataset[dataset["variety"] =="Iris-virginica"]
 
-#print(s)
-#print(ver)
-#print(vir)
-# Prints each description
 
+#Printing description of each variety
+print("Iris setosa:")
+print(setosa.describe())
+
+print("Iris versicolor:")
+print(versicolor.describe())
+
+print("Iris virginica:")
+print(virginica.describe())
+
+
+### --- Visualisation:
 
 dataset.hist(color = "darkslateblue", edgecolor="yellow")
 # Changing colors of histogram
 
-#plt.savefig(fname="variable_histograms.png")
+plt.savefig(fname="histograms.png")
 # Saves the histograms to a .png file
 
-#plt.show()
+sns.pairplot(dataset, hue="variety")
+#plt.savefig(fname="scatterplot.png")
+
+plt.show()
 # Display plot
 
